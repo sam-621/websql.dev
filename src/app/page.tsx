78931be0ca3074c037fcf570d71b0f0ae3aae634
samuel <samuel.corrales621@gmail.com>
2024-12-24 01:58:09 -0700

    feat(layout):  add github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,17 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <div className="grid grid-cols-[auto,1fr]">
-      <aside className="bg-muted h-screen w-fit border-r">
+      <aside className="bg-muted h-screen w-fit border-r flex flex-col justify-between">
         <div className="flex flex-col items-center gap-6 p-3">
           <Image src="/logo.svg" alt="Logo" width={39} height={39} />
           <div className="p-3 bg-muted-foreground/10 rounded-full">
             <DatabaseIcon size={24} className="text-muted-foreground" />
           </div>
+        </div>
+        <div className="p-3 flex justify-center">
+          <a href="https://github.com/sam-621/websql.dev" target="_blank">
+            <Image src="/github.svg" width={32} height={32} alt="Github logo" priority />
+          </a>
         </div>
       </aside>
       <main className="max-w-[calc(100vw-73px)]">
